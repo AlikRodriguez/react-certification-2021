590a4f104e78951a3b4667ad549c79b770df6219
Alik Rodriguez <irwin.rodriguez@wizeline.com>
2021-02-19 15:58:55 -0600

    mini challenge ended

--- src/pages/Home/Home.page.jsx
+++ src/pages/Home/Home.page.jsx
@@ -1,12 +1,32 @@
-import React, { useRef } from 'react';
+import React from 'react';
 import { Link, useHistory } from 'react-router-dom';
-
+import styled from 'styled-components';
+import Grid from '../../components/Grid/Grid.component';
+import Header from '../../components/Header';
+import HomeContent from '../../components/HomeContent/HomeContent.component';
+import Menu from '../../components/Menu';
+import videosMock from '../../providers/videos.mock.json';
 import { useAuth } from '../../providers/Auth';
-import './Home.styles.css';
+import Toggle from '../../components/Toggle';
+import SearchBar from '../../components/SearchBar/SearchBar.component';
+import ProfileMenu from '../../components/ProfileMenu/ProfileMenu.component';
+
+const Main = styled.main`
+  display: flex;
+  flex-grow: 1;
+`;
+
+const Home = styled.section`
+  display: flex;
+  flex-direction: column;
+  width: 100vw;
+  height: 100vh;
+`;
+
+const videos = videosMock.items;
 
 function HomePage() {
   const history = useHistory();
-  const sectionRef = useRef(null);
   const { authenticated, logout } = useAuth();
 
   function deAuthenticate(event) {
@@ -16,23 +36,25 @@ function HomePage() {
   }
 
   return (
-    <section className="homepage" ref={sectionRef}>
-      <h1>Hello stranger!</h1>
+    <Home>
       {authenticated ? (
         <>
-          <h2>Good to have you back</h2>
-          <span>
-            <Link to="/" onClick={deAuthenticate}>
-              ← logout
-            </Link>
-            <span className="separator" />
-            <Link to="/secret">show me something cool →</Link>
-          </span>
+          <Header>
+            <Toggle />
+            <SearchBar />
+            <ProfileMenu />
+          </Header>
+          <Main>
+            <Menu deAuthenticate={deAuthenticate} />
+            <HomeContent>
+              <Grid videos={videos.filter((video) => video.id.videoId)} />
+            </HomeContent>
+          </Main>
         </>
       ) : (
         <Link to="/login">let me in →</Link>
       )}
-    </section>
+    </Home>
   );
 }
 
